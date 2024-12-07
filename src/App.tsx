77c98f984517473b265091220f004406a3c8c4e5
Gabriel Nastase <vonaffenfels@googlemail.com>
2024-12-07 15:15:22 +0100

    INIT

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+      <div>
+          <h1 className="text-4xl">
+              GN-HUB
+          </h1>
+          <span>
+              Time Trackers
+          </span>
+      </div>
+  )
+}
+
+export default App
